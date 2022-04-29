070d2f67ed6645ecf7144c858ac7ca5e89218d1a
edowon0623@gmail.com <Dwlee$09>
2022-04-29 18:52:20 +0900

    upload oracle jdbc drvier

--- src/main/java/com/example/pgdemo/OracleDemo.java
+++ src/main/java/com/example/pgdemo/OracleDemo.java
@@ -90,10 +90,10 @@ public class OracleDemo {
                         "TO_CHAR(SYSDATE, 'YYYYMMDDHH24MISS'))";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, "MONO_CUSTOMER"); // UMS시스템에서 할당 받은 사용자 계정을 입력
-            pstmt.setString(2, new String("[외부전송]SMS")); // [외부전송]SMS
-            pstmt.setString(3, "외부시스템 SMS전송테스트 입니다."); // 외부시스템 SMS전송테스트 입니다.
+            pstmt.setString(2, new String("[External send]SMS")); // [외부전송]SMS
+            pstmt.setString(3, "This is a test message for external system"); // 외부시스템 SMS전송테스트 입니다.
             pstmt.setString(4, "0221558853"); // 021112222
-            pstmt.setString(5, "사회공헌플랫폼"); // 홍길동
+            pstmt.setString(5, "SEOCHo SCP"); // 홍길동
             pstmt.setString(6, "01034912327"); // 01011112222
 
             int result = pstmt.executeUpdate();
